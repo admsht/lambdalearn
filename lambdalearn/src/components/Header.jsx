6318a28b97037572b84ae4fff491b59058310e3c
Adam Shtrikman <69696539+admsht@users.noreply.github.com>
2023-02-18 13:09:00 -0800

    Add to github

---
+++ lambdalearn/src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <div class='bg-gray-900'>
+            <div>
+        <header class="max-w-screen-xl px-4 py-2 mx-auto lg:gap-8 xl:gap-0">
+            <nav class="px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <a href="https://google.com" class="flex items-center">
+                        <span class="self-center text-white text-xl font-semibold whitespace-nowrap dark:text-white">LambdaLearn</span>
+                    </a>
+                </div>
+            </nav>
+        </header>
+        </div>
+        </div>
+    );
+}
+
+export default Header;
