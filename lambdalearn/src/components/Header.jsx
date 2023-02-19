7dea5839ef20b476369d51a359fef858211551c1
Adam Shtrikman <69696539+admsht@users.noreply.github.com>
2023-02-18 17:40:28 -0800

    Changes

--- lambdalearn/src/components/Header.jsx
+++ lambdalearn/src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
     return (
-        <div class='bg-gray-900'>
+        <div class='bg-gray-900 max-w-screen-xl px-4 py-2 mx-auto'>
             <div>
-        <header class="max-w-screen-xl px-4 py-2 mx-auto lg:gap-8 xl:gap-0">
+        <header class="max-w-screen-xl px-4 py-2 mx-auto">
             <nav class="px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                 <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="https://google.com" class="flex items-center">
