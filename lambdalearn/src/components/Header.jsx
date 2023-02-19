f027ffa30d1c9be75b0b33e810fd6ecd47c95ba7
Tom <tomy51451@gmail.com>
2023-02-18 17:39:02 -0800

    updated work

--- lambdalearn/src/components/Header.jsx
+++ lambdalearn/src/components/Header.jsx
@@ -1,12 +1,12 @@
 function Header() {
     return (
-        <div class='bg-gray-900'>
+        <div className='bg-gray-900'>
             <div>
-        <header class="max-w-screen-xl px-4 py-2 mx-auto lg:gap-8 xl:gap-0">
-            <nav class="px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="https://google.com" class="flex items-center">
-                        <span class="self-center text-white text-xl font-semibold whitespace-nowrap dark:text-white">LambdaLearn</span>
+        <header className="max-w-screen-xl px-4 py-2 mx-auto lg:gap-8 xl:gap-0">
+            <nav className="px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <a href="https://google.com" className="flex items-center">
+                        <span className="self-center text-white text-xl font-semibold whitespace-nowrap dark:text-white">LambdaLearn</span>
                     </a>
                 </div>
             </nav>
