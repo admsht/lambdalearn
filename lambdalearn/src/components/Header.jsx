d5e7982c453ff5e0401eadf15385bbb7a102ce96
Adam Shtrikman <69696539+admsht@users.noreply.github.com>
2023-02-18 18:50:18 -0800

    Commit

--- lambdalearn/src/components/Header.jsx
+++ lambdalearn/src/components/Header.jsx
@@ -1,27 +1,17 @@
 function Header() {
     return (
-<<<<<<< HEAD
-        <div class='bg-gray-900 max-w-screen-xl px-4 py-2 mx-auto'>
+        <div class='bg-gray-900 max-w-screen px-4 py-2 mx-auto'>
             <div>
-        <header class="max-w-screen-xl px-4 py-2 mx-auto">
-            <nav class="px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="https://google.com" class="flex items-center">
-                        <span class="self-center text-white text-xl font-semibold whitespace-nowrap dark:text-white">LambdaLearn</span>
-=======
-        <div className='bg-gray-900'>
-            <div>
-        <header className="max-w-screen-xl px-4 py-2 mx-auto lg:gap-8 xl:gap-0">
-            <nav className="px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="https://google.com" className="flex items-center">
-                        <span className="self-center text-white text-xl font-semibold whitespace-nowrap dark:text-white">LambdaLearn</span>
->>>>>>> f027ffa30d1c9be75b0b33e810fd6ecd47c95ba7
-                    </a>
-                </div>
-            </nav>
-        </header>
-        </div>
+                <header class="max-w-screen-xl px-4 py-2 mx-auto">
+                    <nav class="px-4">
+                        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                            <a href="https://google.com" class="flex items-center">
+                                <span class="self-center text-white text-xl font-semibold">Lambda Learn</span>
+                            </a>
+                        </div>
+                    </nav>
+                </header>
+            </div>
         </div>
     );
 }
